1b578a01f7d9e574ab97ce4ce2f436172a4b2d73
Junghee Kim <a01030704572@gmail.com>
2026-09-11 22:19:18 +0900

    Implement alternate linked list merge

--- Linked_List/Q2_A_LL.c
+++ Linked_List/Q2_A_LL.c
@@ -103,7 +103,24 @@ int main()
 
 void alternateMergeLinkedList(LinkedList *ll1, LinkedList *ll2)
 {
-    /* add your code here */
+    ListNode*cur1 = ll1 -> head;
+	ListNode*cur2 = ll2 -> head;
+	int index = 0;
+	while(cur2 != NULL && cur1 != NULL){
+	
+		ListNode *ncur1 = cur1 -> next;
+		ListNode *ncur2 = cur2 -> next;
+		cur1 -> next  = cur2;
+		cur2 -> next = ncur1;
+		cur1 = ncur1;
+		cur2 = ncur2;
+		index ++;
+
+
+	}
+	ll2 -> head = cur2;
+	ll1 -> size += index;
+	ll2 -> size -= index;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
